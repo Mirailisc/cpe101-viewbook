0f11fa7536dd14879a20884ef46b6351918f9af6
Mirailisc <mirailisclm@gmail.com>
2023-10-31 20:09:07 +0700

    transition

--- src/components/SummaryNav/index.tsx
+++ src/components/SummaryNav/index.tsx
@@ -1,15 +1,17 @@
 import styles from './index.module.scss'
 
-const Header: React.FC = (): JSX.Element => {
+const SummaryNav: React.FC = (): JSX.Element => {
   return (
     <div className={styles.navbar}>
+      <a href="/summary">
+        <button>Back</button>
+      </a>
       <h1>CPE101</h1>
       <div className={styles.navlinks}>
         <a href="/">Home</a>
-        <a href="/summary">Summary</a>
       </div>
     </div>
   )
 }
 
-export default Header
+export default SummaryNav
